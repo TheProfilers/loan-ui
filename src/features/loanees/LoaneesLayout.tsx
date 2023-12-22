498838066f95874b97a1f42914137da1ad4d81b3
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-22 14:37:37 +0300

    added login and register

--- src/features/loanees/LoaneesLayout.tsx
+++ src/features/loanees/LoaneesLayout.tsx
@@ -1,4 +1,5 @@
 import BackButton from "../../ui/BackButton";
+import LoaneesDisplay from "./LoaneesDisplay";
 import NewLoanModal from "./NewLoanModal";
 
 export default function LoaneesLayout() {
@@ -8,6 +9,9 @@ export default function LoaneesLayout() {
     <div className="flex justify-between items-start">
     <BackButton />
     <NewLoanModal />
+    </div>
+    <div className="mt-2">
+    <LoaneesDisplay />
     </div>
     </>
   )
