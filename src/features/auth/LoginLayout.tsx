3d6552130ff315b7005ae318bbf9b7566308e2f0
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-22 14:51:20 +0300

    added logic

--- src/features/auth/LoginLayout.tsx
+++ src/features/auth/LoginLayout.tsx
@@ -1,10 +1,19 @@
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 export default function LoginLayout() {
+    const {register,handleSubmit,formState:{errors}} = useForm();
+    const handleLogin = (data: any) => {
+        console.log(data);
+      };
+      const onErrors = (error: any) => {
+        console.log(error);
+        console.log(errors);
+      };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-        <form className="card-body">
+        <form className="card-body" onSubmit={handleSubmit(handleLogin,onErrors)} >
           <div className="form-control">
             <label className="label">
               <span className="label-text">Email</span>
@@ -14,7 +23,11 @@ export default function LoginLayout() {
               placeholder="email"
               className="input input-bordered"
               required
+              {...register("email", { required: "Email is required" })}
             />
+            {errors.email && (
+              <p className="text-red-500 text-xs italic">Enter the Email</p>
+            )}
           </div>
           <div className="form-control">
             <label className="label">
@@ -25,7 +38,11 @@ export default function LoginLayout() {
               placeholder="password"
               className="input input-bordered"
               required
+              {...register("password", { required: "password is required" })}
             />
+            {errors.password && (
+              <p className="text-red-500 text-xs italic">Enter the password</p>
+            )}
             <label className="label">
               <a href="#" className="label-text-alt link link-hover">
                 Forgot password?
