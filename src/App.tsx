c11373186bfb7878b910a6ac434c9a515d01cd50
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-22 14:37:18 +0300

    added login and register

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Loanees from "./pages/Loanees";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import AppLayout from "./ui/AppLayout";
+import ProtectedRoute from "./ui/ProtectedRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<AppLayout />}>
+        <Route element={
+          <ProtectedRoute>
+            <AppLayout />
+          </ProtectedRoute>
+        }>
           <Route path="/" element={<Home />} />
           <Route path="/loanees" element={<Loanees />} />
         </Route>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="*" element={<div>404</div>} />
+
       </Routes>
     </BrowserRouter>
   );
