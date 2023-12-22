03f87e16b331549c3e15f9e6ed32d3f210094c57
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-22 14:37:32 +0300

    added login and register

---
+++ src/features/loanees/LoaneesDisplay.tsx
@@ -0,0 +1,36 @@
+import { BsFillEyeFill, BsTrash3 } from "react-icons/bs";
+export default function LoaneesDisplay() {
+  return (
+    <div className="overflow-x-auto">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* row 1 */}
+          <tr className="bg-base-200">
+            <th>1</th>
+            <td>Brian</td>
+            <td>1234567890</td>
+            <td>
+              <div className="flex space-x-1">
+                <button className="btn btn-sm btn-square btn-success">
+                  <BsFillEyeFill />
+                </button>
+                <button className="btn btn-sm btn-square btn-warning">
+                  <BsTrash3 />
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
