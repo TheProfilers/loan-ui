3fd13bfdfa4b5c01223b6c5509baa89c14318d1c
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-25 12:41:40 +0300

    implimenting fetch logic

---
+++ src/ui/ReusableModal.tsx
@@ -0,0 +1,28 @@
+import { ReactNode, useRef } from 'react';
+
+export default function ReusableModal({children,title}:{children:ReactNode,title:string}) {
+    const newLoanModal = useRef<HTMLDialogElement>(null);
+  const openModal = () => {
+    newLoanModal.current?.showModal();
+  };
+  const closeModal = () => {
+    newLoanModal.current?.close();
+  };
+  return (
+    <>
+    
+<button type='button' className="btn" onClick={openModal}>{title}</button>
+<dialog ref={newLoanModal} className="modal">
+  <div className="modal-box">
+    <div className="flex justify-end">
+      <button type="button" onClick={closeModal} className="text-2xl cursor-pointer">
+        X
+      </button>
+    </div>
+    {children}
+   
+  </div>
+</dialog>
+    </>
+  )
+}
