a4bdbaaaca0724012304447762b59fcd1f0ae27a
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-26 02:39:32 +0300

    display all agents

--- src/services/shopagentapi.ts
+++ src/services/shopagentapi.ts
@@ -1,42 +1,40 @@
-import { UserTypes } from "../types/UserTypes"
+import { UserTypes } from "../types/UserTypes";
 
-const BASE_URL = "http://localhost:3000/"
+const BASE_URL = "http://localhost:3000/";
 
-export async function getAllAgents(){
-try {
-    const response = await fetch(`${BASE_URL}agents`,
-    {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("token")}`
-        }
-    })
-    const data = await response.json()
-    return data
-    
-    
-} catch (error:any) {
-    throw new Error(error.message)
-}
-}
-
-export async function newAgent(userData:UserTypes){
-    try {
-        const response = await fetch(`${BASE_URL}auth/signup`,{
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(userData)
-        })
-        const data = await response.json()
-        if(data.statusCode === 500){
-            throw new Error(data.message)
-        }
-        return data
-       
-    } catch (error:any) {
-        throw new Error(error.message)
+export async function getAllAgents() {
+  try {
+    const response = await fetch(`${BASE_URL}users`, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(localStorage.getItem("token")!)}`,
+      },
+    });
+   
+    if (response.status === 401) {
+      throw new Error("You are not authorized to perform this action");
     }
+    const data = await response.json();
+    return data;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+}
+
+export async function newAgent(userData: UserTypes) {
+  try {
+    const response = await fetch(`${BASE_URL}auth/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+    const data = await response.json();
+    if (data.statusCode === 500) {
+      throw new Error(data.message);
+    }
+    return data;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
 }
