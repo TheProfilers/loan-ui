63374e6599869832c0c2b478a3aacdfb7cce4766
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-26 22:43:45 +0300

    loanee display

---
+++ src/types/LoaneeType.ts
@@ -0,0 +1,10 @@
+export interface LoaneesType {
+    _id?:string;
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    idNumber: string;
+    email: string;
+    limit: number;
+    
+}
