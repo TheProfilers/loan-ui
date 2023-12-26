206d67946e258047cf19643f20b27000193de027
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-27 00:30:37 +0300

    completed adding new loanee

--- src/services/loaneesapi.ts
+++ src/services/loaneesapi.ts
@@ -1,3 +1,5 @@
+import { LoaneesType } from "../types/LoaneeType";
+
 const BASE_URL = "http://localhost:3000/";
 export async function getAllLoanees() {
   try {
@@ -51,6 +53,32 @@ export async function deleteLoanee(id: string) {
       headers: {
         Authorization: `Bearer ${JSON.parse(localStorage.getItem("token")!)}`,
       },
+    });
+    const data = await response.json();
+    if (data.statusCode === 500) {
+      throw new Error(data.message);
+    }
+    if (response.status === 401) {
+      throw new Error("You are not authorized to perform this action");
+    }
+    if (data.statusCode) {
+      throw new Error(data.message);
+    }
+    return data;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+}
+
+export async function addLoanee(loanee: LoaneesType) {
+  try {
+    const response = await fetch(`${BASE_URL}loanee/new`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${JSON.parse(localStorage.getItem("token")!)}`,
+      },
+      body: JSON.stringify(loanee),
     });
     const data = await response.json();
     if (data.statusCode === 500) {
