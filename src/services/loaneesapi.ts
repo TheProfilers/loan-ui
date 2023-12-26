cdd7bf4a1910e197dbe0cd8cf9f4a1d7f74709fa
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-27 00:07:40 +0300

    completed delete capability of loanee

--- src/services/loaneesapi.ts
+++ src/services/loaneesapi.ts
@@ -1,4 +1,3 @@
-
 const BASE_URL = "http://localhost:3000/";
 export async function getAllLoanees() {
   try {
@@ -14,8 +13,8 @@ export async function getAllLoanees() {
     if (response.status === 401) {
       throw new Error("You are not authorized to perform this action");
     }
-    if(data.statusCode){
-        throw new Error(data.message);
+    if (data.statusCode) {
+      throw new Error(data.message);
     }
     return data;
   } catch (error: any) {
@@ -37,8 +36,31 @@ export async function getLoaneeById(id: string) {
     if (response.status === 401) {
       throw new Error("You are not authorized to perform this action");
     }
-    if(data.statusCode){
-        throw new Error(data.message);
+    if (data.statusCode) {
+      throw new Error(data.message);
+    }
+    return data;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+}
+export async function deleteLoanee(id: string) {
+  try {
+    const response = await fetch(`${BASE_URL}loanee/delete/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${JSON.parse(localStorage.getItem("token")!)}`,
+      },
+    });
+    const data = await response.json();
+    if (data.statusCode === 500) {
+      throw new Error(data.message);
+    }
+    if (response.status === 401) {
+      throw new Error("You are not authorized to perform this action");
+    }
+    if (data.statusCode) {
+      throw new Error(data.message);
     }
     return data;
   } catch (error: any) {
