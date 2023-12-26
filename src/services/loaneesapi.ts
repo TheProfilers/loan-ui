91871b82de893e6f0ba0bf1567c1b1868c2bfcf8
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-26 23:47:08 +0300

    loanee details display

--- src/services/loaneesapi.ts
+++ src/services/loaneesapi.ts
@@ -22,3 +22,26 @@ export async function getAllLoanees() {
     throw new Error(error.message);
   }
 }
+
+export async function getLoaneeById(id: string) {
+  try {
+    const response = await fetch(`${BASE_URL}loanee/${id}`, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(localStorage.getItem("token")!)}`,
+      },
+    });
+    const data = await response.json();
+    if (data.statusCode === 500) {
+      throw new Error(data.message);
+    }
+    if (response.status === 401) {
+      throw new Error("You are not authorized to perform this action");
+    }
+    if(data.statusCode){
+        throw new Error(data.message);
+    }
+    return data;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+}
