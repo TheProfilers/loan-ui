c83036a32dba62a02bda9c04f19c04d10a2dbc0c
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-26 22:43:26 +0300

    loanee display

--- src/features/loanees/LoaneesDisplay.tsx
+++ src/features/loanees/LoaneesDisplay.tsx
@@ -1,5 +1,28 @@
 import { BsFillEyeFill, BsTrash3 } from "react-icons/bs";
+import Loader from "../../ui/Loader";
+import { useAllLoanees } from "./useAllLoanees";
 export default function LoaneesDisplay() {
+  const { data, isLoading, error } = useAllLoanees();
+  if (isLoading) {
+    return <Loader />;
+  }
+  if (error) {
+    return (
+      <div className="h-[calc(100vh-9rem)] flex flex-col justify-center items-center">
+        <div className="shadow p-2">
+        <h1 className="text-2xl text-red-600">{error.message}</h1>
+        </div>
+      </div>
+    );
+  }
+  if (data?.length === 0) {
+    return (
+      <div className="h-screen flex flex-col justify-center items-center">
+        <h1 className="text-2xl">No Agents Found</h1>
+      </div>
+    );
+  }
+  console.log(data);
   return (
     <div className="overflow-x-auto">
       <table className="table">
