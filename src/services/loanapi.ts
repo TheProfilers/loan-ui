8ded5ce85b37bde23dfd989a0adfb0dbca9eaff9
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-27 01:18:52 +0300

    fetch loanee loaness

---
+++ src/services/loanapi.ts
@@ -0,0 +1,27 @@
+const BASE_URL = "http://localhost:3000/";
+
+export async function getLoaneeLoans(loanee:string){
+    try {
+        const response = await fetch(`${BASE_URL}loans/loanee/${loanee}`, {
+          
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${JSON.parse(localStorage.getItem("token")!)}`,
+          },
+          
+        });
+        const data = await response.json();
+        if (data.statusCode === 500) {
+          throw new Error(data.message);
+        }
+        if (response.status === 401) {
+          throw new Error("You are not authorized to perform this action");
+        }
+        if (data.statusCode) {
+          throw new Error(data.message);
+        }
+        return data;
+      } catch (error: any) {
+        throw new Error(error.message);
+      }
+}
