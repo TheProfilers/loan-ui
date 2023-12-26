3e8da8e7910291234a6adfcc41bd0bd183e6ca78
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-27 01:49:41 +0300

    display loanee loans

--- src/features/loans/useLoaneeLoans.ts
+++ src/features/loans/useLoaneeLoans.ts
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { getLoaneeLoans } from "../../services/loanapi";
+import { LoanType } from "../../types/LoanType";
 
 export function useLoaneeLoans(){
     const {id} = useParams<{id:string}>()
-    const{data:loans,error:loaneeError,isLoading:isLoadingLoans} = useQuery({
+    const{data:loans,error:loaneeError,isLoading:isLoadingLoans} = useQuery<LoanType[],Error>({
         queryKey:["loaneeLoans"],
         queryFn: ()=>getLoaneeLoans(id!)
     })
