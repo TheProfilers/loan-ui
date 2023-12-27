0fa3937501b75bf76c9b05f4e6ddd065aa055bf4
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-28 00:12:28 +0300

    completed all loans display

--- src/features/loans/LoansLayout.tsx
+++ src/features/loans/LoansLayout.tsx
@@ -1,6 +1,49 @@
+import { BsFillEyeFill, BsTrash3 } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import Loader from "../../ui/Loader";
+import { useAllLoans } from "./useAllLoans";
 
 export default function LoansLayout() {
+    const { data, isLoading, error } = useAllLoans();
+    if (isLoading) return <Loader/>;
+    if (error) return <div>{error.message}</div>;
+    if (!data) return <div>Something went wrong</div>;
+    console.log(data);
   return (
-    <div>LoansLayout</div>
+    <div className="overflow-x-auto">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Loanee</th>
+            <th>Amount</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* row 1 */}
+         {
+            data?.map((loan,index) => (
+                <tr className="bg-base-200" key={index} >
+                <th>{index + 1}</th>
+                <td>{loan.loanee.firstName}</td>
+                <td>{loan.loanAmount}</td>
+                <td>
+                  <div className="flex space-x-1">
+                    <Link to={`/loans/${loan._id}`} className="btn btn-sm btn-square btn-success">
+                      <BsFillEyeFill />
+                    </Link>
+                    <button  className="btn btn-sm btn-square btn-warning">
+                      <BsTrash3 />
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))
+         }
+        </tbody>
+      </table>
+    </div>
   )
 }
