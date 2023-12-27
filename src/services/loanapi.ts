f7e347589273d91672baff6489b89e2b74e7f3a8
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-27 04:05:25 +0300

    borrow loan working

--- src/services/loanapi.ts
+++ src/services/loanapi.ts
@@ -1,3 +1,5 @@
+import { LoanType } from "../types/LoanType";
+
 const BASE_URL = "http://localhost:3000/";
 
 export async function getLoaneeLoans(loanee:string){
@@ -33,6 +35,32 @@ export async function getLoanById(id: string) {
       headers: {
         Authorization: `Bearer ${JSON.parse(localStorage.getItem("token")!)}`,
       },
+    });
+    const data = await response.json();
+    if (data.statusCode === 500) {
+      throw new Error(data.message);
+    }
+    if (response.status === 401) {
+      throw new Error("You are not authorized to perform this action");
+    }
+    if (data.statusCode) {
+      throw new Error(data.message);
+    }
+    return data;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+}
+
+export async function newLoan(loan: LoanType) {
+  try {
+    const response = await fetch(`${BASE_URL}loans/new`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${JSON.parse(localStorage.getItem("token")!)}`,
+      },
+      body: JSON.stringify(loan),
     });
     const data = await response.json();
     if (data.statusCode === 500) {
