fac7fbce1cd1eea8bf924953257d598227523fb3
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-28 01:41:06 +0300

    performed role based authorization

--- src/ui/ProtectedRoute.tsx
+++ src/ui/ProtectedRoute.tsx
@@ -1,15 +1,13 @@
 import { ReactNode, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import Agent from "../pages/Agent";
 
-export default function ProtectedRoute({children}:{children:ReactNode}) {
-    const navigate = useNavigate();
-    const{storedUser} = useAuth();
-    console.log(storedUser)
-    useEffect(()=>{
-        if(!storedUser) navigate('/login')
-
-    },[storedUser,navigate])
-  return storedUser?.role == 'admin' ? children : storedUser?.role == 'agent' ? <Agent/> : null;
+export default function ProtectedRoute({ children }: { children: ReactNode }) {
+  const navigate = useNavigate();
+  const { storedUser } = useAuth();
+  console.log(storedUser);
+  useEffect(() => {
+    if (!storedUser) navigate("/login");
+  }, [storedUser, navigate]);
+  return storedUser ? children : null;
 }
