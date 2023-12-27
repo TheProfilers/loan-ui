3c01d897702a495c4d5d3608833dd950dd63f2ed
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-27 03:56:44 +0300

    borrow loan

--- src/features/loans/NewLoanModal.tsx
+++ src/features/loans/NewLoanModal.tsx
@@ -1,9 +1,74 @@
+import { useForm } from "react-hook-form";
+import { useParams } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import { LoanType } from "../../types/LoanType";
 import ReusableModal from "../../ui/ReusableModal";
 
 export default function NewLoanModal() {
+  const {id} = useParams<{id:string}>();
+  const {storedUser} = useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const handleNewLoan = (data: any) => {
+    const loanData:LoanType = {
+      loanAmount: data.loanAmount,
+      amountPaid: 0,
+      loanReason: data.loanReason,
+      loanee: id!,
+      servedBy: storedUser?.id!,
+
+    }
+    console.log(loanData);
+    console.log(storedUser);
+    
+  };
+  const onErrors = (error: any) => {
+    console.log(error);
+    console.log(errors);
+  };
   return (
     <ReusableModal title="Give Out Loan">
-        <p>You are about to give loan</p>
+      <form
+        className="card-body"
+        onSubmit={handleSubmit(handleNewLoan, onErrors)}
+      >
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Loan Amount</span>
+          </label>
+          <input
+            type="number"
+            className="input input-bordered"
+            {...register("loanAmount", { required: "Loan Amount is required",min: 0,valueAsNumber:true })}
+          />
+          {errors.loanAmount && (
+            <p className="text-red-500 text-xs italic">
+              Enter the Amount of Loan
+            </p>
+          )}
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Loan Reason</span>
+          </label>
+          <textarea
+            className="input input-bordered"
+            {...register("loanReason", { required: "Loan Reason is required" })}
+          ></textarea>
+          {errors.loanReason && (
+            <p className="text-red-500 text-xs italic">Enter the Loan Reason</p>
+          )}
+        </div>
+
+        <div className="form-control mt-6">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
     </ReusableModal>
-  )
+  );
 }
