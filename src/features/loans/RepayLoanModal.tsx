9ca1a872228ffd91b97af2315f5f7dfd29c2a2a8
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-27 03:05:50 +0300

    show repay laon conditionally

---
+++ src/features/loans/RepayLoanModal.tsx
@@ -0,0 +1,9 @@
+import ReusableModal from '../../ui/ReusableModal'
+
+export default function RepayLoanModal() {
+  return (
+    <ReusableModal title='Repay Loan'>
+        <p>You are repaying the loan</p>
+    </ReusableModal>
+  )
+}
