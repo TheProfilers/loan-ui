b70257d8c7d263823b23f6140a6eca5dd685d007
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-27 04:15:44 +0300

    loan repayment done

--- src/features/loans/RepayLoanModal.tsx
+++ src/features/loans/RepayLoanModal.tsx
@@ -1,9 +1,52 @@
-import ReusableModal from '../../ui/ReusableModal'
+import { useForm } from 'react-hook-form';
+import ReusableModal from '../../ui/ReusableModal';
+import { useRepayLoan } from './useRepayLoan';
 
 export default function RepayLoanModal() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset
+  } = useForm();
+  const {mutate,isPending} = useRepayLoan();
+  const handleRepayLoan = (data: any) => {
+    mutate(data.loanAmount)
+    reset();
+  };
+  const onErrors = (error: any) => {
+    console.log(error);
+    console.log(errors);
+  };
   return (
     <ReusableModal title='Repay Loan'>
-        <p>You are repaying the loan</p>
+        <form
+        className="card-body"
+        onSubmit={handleSubmit(handleRepayLoan, onErrors)}
+      >
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Loan Amount</span>
+          </label>
+          <input
+            type="number"
+            className="input input-bordered"
+            {...register("loanAmount", { required: "Loan Amount is required",min: 0,valueAsNumber:true })}
+          />
+          {errors.loanAmount && (
+            <p className="text-red-500 text-xs italic">
+              Enter the Amount of Loan
+            </p>
+          )}
+        </div>
+        
+
+        <div className="form-control mt-6">
+          <button disabled={isPending} type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
     </ReusableModal>
   )
 }
