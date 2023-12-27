a5b2770952b8765726ae20331c292b49e6f690c0
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-27 03:56:56 +0300

    borrow loan

--- src/types/LoanTypes.ts
+++ src/types/LoanTypes.ts
@@ -4,6 +4,7 @@ import { UserTypes } from "./UserTypes";
 export interface LoaneTypes{
     _id: string;
     loanAmount:number
+    amountPaid:number
     totalLoanAmount?:number
     loanReason:string
     servedBy:UserTypes
