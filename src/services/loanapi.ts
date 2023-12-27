ab2c8e8113c642886a350b43d2d4e580e16b0379
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-28 01:56:33 +0300

    delete loan logic

--- src/services/loanapi.ts
+++ src/services/loanapi.ts
@@ -126,3 +126,27 @@ export async function getAllLoans() {
     throw new Error(error.message);
   }
 }
+
+export async function deleteLoan(id: string) {
+  try {
+    const response = await fetch(`${BASE_URL}loans/loan/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${JSON.parse(localStorage.getItem("token")!)}`,
+      },
+    });
+    const data = await response.json();
+    if (data.statusCode === 500) {
+      throw new Error(data.message);
+    }
+    if (response.status === 401) {
+      throw new Error("You are not authorized to perform this action");
+    }
+    if (data.statusCode) {
+      throw new Error(data.message);
+    }
+    return data;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+}
