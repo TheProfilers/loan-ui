b9f73889234fb694f1da7edf4ad7fcc3c0a213b7
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-28 05:31:33 +0300

    implimented graphs

--- src/services/loaneesapi.ts
+++ src/services/loaneesapi.ts
@@ -95,3 +95,26 @@ export async function addLoanee(loanee: LoaneesType) {
     throw new Error(error.message);
   }
 }
+
+export async function getLoansAfterDate(date:string){
+  try {
+    const response = await fetch(`${BASE_URL}loans/after/${date}`, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(localStorage.getItem("token")!)}`,
+      },
+    });
+    const data = await response.json();
+    if (data.statusCode === 500) {
+      throw new Error(data.message);
+    }
+    if (response.status === 401) {
+      throw new Error("You are not authorized to perform this action");
+    }
+    if (data.statusCode) {
+      throw new Error(data.message);
+    }
+    return data;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+}
