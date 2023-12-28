fde572245ce58644b473f4ec8bb4bfd438c8fdd4
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-29 02:48:26 +0300

    figured out download

--- src/features/loans/LoaneeLoans.tsx
+++ src/features/loans/LoaneeLoans.tsx
@@ -1,26 +1,33 @@
 import { Link } from "react-router-dom"
 import { LoanType } from "../../types/LoanType"
+import { LoaneTypes } from "../../types/LoanTypes"
 import ColumnText from "../../ui/ColumnText"
 import Loader from "../../ui/Loader"
 import { useLoaneeDetails } from "../loanees/useLoaneeDetails"
+import { useSettings } from "../settings/useSettings"
 import NewLoanModal from "./NewLoanModal"
+import { useAllLoans } from "./useAllLoans"
 import { useLoaneeLoans } from "./useLoaneeLoans"
 
 export default function LoaneeLoans() {
   
     const {loans,loaneeError,isLoadingLoans} = useLoaneeLoans()
+    const { data:allLoans} = useAllLoans();
+    const {data} = useSettings()
     const {loanee}= useLoaneeDetails()
     if(isLoadingLoans) return <Loader/>
     if(loaneeError) return <div className="my-3 px-4 text-sm text-red-600 font-medium">{loaneeError.message}</div>
-    if(!loans) return <div>Something went wrong</div>
+    if(!loans || !allLoans || !data) return <div>Something went wrong</div>
     const totalAmount = loans.reduce((acc:number,loan:LoanType)=>acc+loan.loanAmount,0)
+    const totalLoansAmount = allLoans.reduce((acc:number,loan:LoaneTypes)=>acc+loan.loanAmount,0)
     if(!loanee) return <div>Something went wrong</div>
     console.log(loans)
   return (
     <>
-    <div className="flex justify-end mt-3">
+   {data.loansLimit > totalLoansAmount && <div className="flex justify-end mt-3">
     {(totalAmount < loanee?.limit - 1) && <NewLoanModal/>}
-    </div>
+    </div>}
+    
     <div className="shadow p-4 rounded-sm">
     <ColumnText title="Total Loans" text={loans.length.toString()}/>
     <ColumnText title="Total Amount of Loan" text={totalAmount.toString()}/>
