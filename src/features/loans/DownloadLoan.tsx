bee605db210d522202b88fc15c59e38ce142c1d2
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-29 02:48:16 +0300

    figured out download

--- src/features/loans/DownloadLoan.tsx
+++ src/features/loans/DownloadLoan.tsx
@@ -1,64 +1,100 @@
-import { Document, Page, StyleSheet, Text, View } from '@react-pdf/renderer';
-import { LoaneTypes } from '../../types/LoanTypes';
+import domToImage from 'dom-to-image';
+import jsPDF from 'jspdf';
+import { useState } from 'react';
+import ColumnText from '../../ui/ColumnText';
+import Loader from '../../ui/Loader';
+import './download.css';
+import { useLoanDetails } from './useLoanDetails';
 
-const styles = StyleSheet.create({
-  body: {
-    paddingTop: 35,
-    paddingBottom: 65,
-    paddingHorizontal: 35,
-  },
-  title: {
-    fontSize: 24,
-    textAlign: 'center',
-    
-  },
-  author: {
-    fontSize: 12,
-    textAlign: 'center',
-    marginBottom: 40,
-  },
-  subtitle: {
-    fontSize: 18,
-    margin: 12,
-    fontFamily: 'Oswald'
-  },
-  text: {
-    margin: 12,
-    fontSize: 14,
-    textAlign: 'justify',
-    
-  },
-  image: {
-    marginVertical: 15,
-    marginHorizontal: 100,
-  },
-  header: {
-    fontSize: 12,
-    marginBottom: 20,
-    textAlign: 'center',
-    color: 'grey',
-  },
-  pageNumber: {
-    position: 'absolute',
-    fontSize: 12,
-    bottom: 30,
-    left: 0,
-    right: 0,
-    textAlign: 'center',
-    color: 'grey',
-  },
-});
 
-export default function DownloadLoan({loanData}: {loanData: LoaneTypes}) {
-    console.log(loanData);
+export default function DownloadLoan() {
+  const [isDownloading, setIsDownloading] = useState(false);
+  const downloadPDF = async () => {
+    const capture = document.querySelector('.actual-receipt') as HTMLElement;
+  
+    if (capture) {
+      setIsDownloading(true);
+      try {
+        const dataUrl = await domToImage.toPng(capture);
+        
+        const img = new Image();
+        img.src = dataUrl;
+
+        const doc = new jsPDF('p', 'px', 'a4',true);
+        const width = doc.internal.pageSize.getWidth();
+        const height = doc.internal.pageSize.getHeight();
+        doc.addImage(img, 'PNG', 0, 0, width, height);
+        doc.save('download.pdf');
+        setIsDownloading(false);
+      } catch (error) {
+        setIsDownloading(false);
+        console.error('Error capturing element:', error);
+      }
+    } else {
+      setIsDownloading(false);
+      console.error('Element not found.');
+    }
+  };
+    const { loan, error, isLoading } = useLoanDetails();
+    if (isLoading) {
+      return <Loader />;
+    }
+    if (error) {
+      return <p className="text-red-500">{error.message}</p>;
+    }
+    if (!loan) {
+      return <p>No loan found</p>;
+    }
+    console.log(loan);
+   
+   
   return (
-    <Document>
-    <Page>
-    <View style={{ color: 'white', textAlign: 'center', margin: 30 }}>
-        <Text>Section #1</Text>
-      </View>
-    </Page>
-  </Document>
+    <>
+    <div className='actual-receipt'>
+      
+    
+      <h1 className="text-lg font-medium text-orange-500 uppercase px-4">
+        Loan Information
+      </h1>
+
+      <div className="md:flex justify-between shadow p-3">
+        <div>
+          <ColumnText title="Loan Amount" text={loan.loanAmount.toString()} />
+          <ColumnText title="Loan Reason" text={loan.loanReason} />
+        </div>
+        <div>
+          <ColumnText
+            title="Loan Balance"
+            text={(loan.loanAmount - loan.amountPaid).toString()}
+          />
+          <ColumnText title="Amount Paid" text={loan.amountPaid.toString()} />
+        </div>
+      </div>
+
+      <h1 className="text-lg font-medium text-orange-500 uppercase px-4">
+        Loanee Information
+      </h1>
+
+      <div className="shadow grid grid-cols-2 md:grid-cols-4 gap-8 p-4 mt-2">
+        <ColumnText title="First name" text={loan.loanee.firstName} />
+        <ColumnText title="Last name" text={loan.loanee.lastName} />
+        <ColumnText title="Id Number" text={loan.loanee.idNumber} />
+        <ColumnText title="Phone Number" text={loan.loanee.phoneNumber} />
+      </div>
+      <h1 className="text-lg font-medium text-orange-500 uppercase px-4">
+        Agent Information
+      </h1>
+
+      <div className="shadow grid grid-cols-2 md:grid-cols-4 gap-8 p-4 mt-2">
+        <ColumnText title="Full Name" text={loan.servedBy.name} />
+        <ColumnText title="Phone Number" text={loan.servedBy.phone!} />
+        <ColumnText title="Email" text={loan.servedBy.email} />
+      </div>
+     
+      
+    </div>
+    <button className='btn btn-primary mt-2' disabled={isDownloading} onClick={downloadPDF}>{isDownloading ? 'Downloading': 'Download PDF'}</button>
+    </>
 
   )
 }
