35e8aa4104d4f803bcdf1d556e05822a86e58edc
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-29 17:06:40 +0300

    completed the loan errors

--- src/features/loans/useDeleteLoan.ts
+++ src/features/loans/useDeleteLoan.ts
@@ -5,16 +5,20 @@ import { deleteLoan } from "../../services/loanapi";
 export function useDeleteLoan(){
     const queryClient = useQueryClient();
     const {mutate,isPending} = useMutation({
-        mutationFn:deleteLoan,
+        mutationFn:(id:string)=>deleteLoan(id),
         onSuccess:()=>{
             queryClient.invalidateQueries();
             Swal.fire({
-                icon:'success',
-                title:'Loan deleted successfully'
-            
+                icon:"success",
+                title:"Loan deleted successfully",
+                toast:true,
+                position:"top-right",
+                showConfirmButton:false,
+                timer:3000
             })
         },
         onError:(error:Error)=>{
+            console.log(error)
             Swal.fire({
                 icon:'error',
                 title:'Oops...',
