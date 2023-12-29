c10f826deb365a873fc34f2f2a2317ef86031ac2
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-29 15:43:28 +0300

    modified loan received page

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,8 @@
+import { format, parseISO } from "date-fns";
+
 export const formatCurrency = (value:number) =>
   new Intl.NumberFormat('en', { style: 'currency', currency: 'KES' }).format(
     value
   );
+
+  export const formatDate = (dateStr:string) => format(parseISO(dateStr), 'dd MMM yyyy HH:mm');
