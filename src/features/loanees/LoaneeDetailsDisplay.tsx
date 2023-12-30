dd06949b5ca05c146ad6c94d599c6e92599e946e
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-31 00:14:01 +0300

    table display

--- src/features/loanees/LoaneeDetailsDisplay.tsx
+++ src/features/loanees/LoaneeDetailsDisplay.tsx
@@ -1,5 +1,5 @@
-import ColumnText from "../../ui/ColumnText";
 import Loader from "../../ui/Loader";
+import { formatCurrency } from "../../utils/helpers";
 import { useLoaneeDetails } from "./useLoaneeDetails";
 
 export default function LoaneeDetailsDisplay() {
@@ -13,15 +13,35 @@ export default function LoaneeDetailsDisplay() {
     <>
       <h3 className="font-medium text-lg my-3 text-orange-500">Loanee Information</h3>
       <div className="shadow rounded-sm p-2">
-        <ColumnText title="Email" text={loanee.email} />
-        <div className="mt-2 grid grid-cols-2 md:grid-cols-4 gap-8">
+        <div className="overflow-x-auto">
+          <table className="table sm:table-xs">
+          <thead>
+            <tr>
+              <th>Borrower</th>
+              <th>Id No.</th>
+              <th>Phone</th>
+              <th>Limit</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{loanee.firstName}</td>
+              <td>{loanee.idNumber}</td>
+              <td>{loanee.phoneNumber}</td>
+              <td>{formatCurrency(loanee.limit)}</td>
+            </tr>
+          </tbody>
+          </table>
+        </div>
+        {/* <ColumnText title="Email" text={loanee.email} /> */}
+        {/* <div className="mt-2 grid grid-cols-2 md:grid-cols-4 gap-8">
          
           <ColumnText title="First Name" text={loanee.firstName} />
           <ColumnText title="Last Name" text={loanee.lastName} />
           <ColumnText title="Phone" text={loanee.phoneNumber} />
           <ColumnText title="Limit" text={loanee.limit.toString()} />
           <ColumnText title="ID Number" text={loanee.idNumber} />
-        </div>
+        </div> */}
       </div>
     </>
   );
