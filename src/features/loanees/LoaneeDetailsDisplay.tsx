b7e34eaea44f976e292c9c81c5294bbb8b76f37b
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-31 01:51:35 +0300

    loan details

--- src/features/loanees/LoaneeDetailsDisplay.tsx
+++ src/features/loanees/LoaneeDetailsDisplay.tsx
@@ -1,9 +1,12 @@
+import { useAuth } from "../../context/AuthContext";
 import Loader from "../../ui/Loader";
 import { formatCurrency } from "../../utils/helpers";
+import UpdateLoaneeLimit from "./UpdateLoaneeLimit";
 import { useLoaneeDetails } from "./useLoaneeDetails";
 
 export default function LoaneeDetailsDisplay() {
     const {loanee, error, isLoading}= useLoaneeDetails()
+    const{storedUser} = useAuth();
    
     if(isLoading) return <Loader/>
     if(error ) return  <div>{error.message}</div> 
@@ -11,7 +14,10 @@ export default function LoaneeDetailsDisplay() {
    
   return (
     <>
+      <div className="flex justify-between">
       <h3 className="font-medium text-lg my-3 text-orange-500">Loanee Information</h3>
+     {storedUser?.role === 'admin' && <UpdateLoaneeLimit />}
+      </div>
       <div className="shadow rounded-sm p-2">
         <div className="overflow-x-auto">
           <table className="table sm:table-xs">
