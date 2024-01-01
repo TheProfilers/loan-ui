4e0561799411acea3ac59d76d188af146b5f35b4
Brian-mula <mulatiwangilabrian@gmail.com>
2024-01-02 00:57:05 +0300

    modified display

--- src/features/loans/LoanDetailsLayout.tsx
+++ src/features/loans/LoanDetailsLayout.tsx
@@ -6,7 +6,6 @@ import RepayLoanModal from "./RepayLoanModal";
 import { useLoanDetails } from "./useLoanDetails";
 
 export default function LoanDetailsLayout() {
- 
   const { loan, error, isLoading } = useLoanDetails();
   if (isLoading) {
     return <Loader />;
@@ -20,118 +19,130 @@ export default function LoanDetailsLayout() {
   console.log(loan);
   return (
     <>
-    <div className='actual-receipt'>
-      
-      <div className="flex justify-between">
-        <BackButton />
+      <div className="actual-receipt">
+        <div className="flex justify-between">
+          <BackButton />
 
-        {loan.loanAmount !== loan.amountPaid && <RepayLoanModal />}
+          {loan.loanAmount !== loan.amountPaid && <RepayLoanModal />}
+        </div>
+        <h1 className="text-sm font-medium text-orange-500 uppercase my-2">
+          Borrower Information
+        </h1>
+        {loan.loanee ? (
+          <div className="overflow-x-auto">
+            <table className="table table-xs">
+              <thead>
+                <tr>
+                  <th>Full Name</th>
+                  <th>Id No.</th>
+                  <th>Phone</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    {loan.loanee.firstName} {loan.loanee.firstName}
+                  </td>
+                  <td>{loan.loanee.idNumber}</td>
+                  <td>{loan.loanee.phoneNumber}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <div>Not Available</div>
+        )}
+        <h1 className="text-sm font-medium text-orange-500 uppercase my-2">
+          Loan Information
+        </h1>
+        <div className="overflow-x-auto">
+          <table className="table table-xs">
+            <thead>
+              <tr>
+                <th>Loan Amount</th>
+                <th>Date Borrowed</th>
+                <th>Loan Reason</th>
+                <th>Amount Paid</th>
+                <th>Date Paid</th>
+                <th>Loan Balance</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{formatCurrency(loan.loanAmount).toString()}</td>
+                <td>{formatDate(loan.createdAt!)}</td>
+                <td>{loan.loanReason}</td>
+                <td>{formatCurrency(loan.amountPaid).toString()}</td>
+                {loan.receivedBy ? (
+                  <td>{formatDate(loan.updatedAt!)}</td>
+                ) : (
+                  <td>Not Paid</td>
+                )}
+                <td>
+                  {formatCurrency(loan.loanAmount - loan.amountPaid).toString()}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <h1 className="text-sm font-medium text-orange-500 uppercase py-1 my-2">
+          Served By
+        </h1>
+
+        {loan.servedBy ? (
+          <div className="overflow-x-auto">
+            <table className="table table-xs">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Phone</th>
+                  <th>Email</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{loan.servedBy.name}</td>
+                  <td>{loan.servedBy.phone}</td>
+                  <td>{loan.servedBy.email}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <div>Not Available</div>
+        )}
+
+        <h1 className="text-sm font-medium text-orange-500 uppercase py-1 my-2">
+          Payment Recipient Details
+        </h1>
+
+        {loan.receivedBy ? (
+          <div className="overflow-x-auto">
+            <table className="table table-xs">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Phone</th>
+                  <th>Email</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{loan.servedBy.name}</td>
+                  <td>{loan.servedBy.phone}</td>
+                  <td>{loan.servedBy.email}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <div>Not Available</div>
+        )}
       </div>
-      <h1 className="text-sm font-medium text-orange-500 uppercase">
-        Borrower Information
-      </h1>
-     {loan.loanee? <div className="overflow-x-auto">
-        <table className="table table-xs">
-        <thead>
-          <tr>
-            <th>Full Name</th>
-            <th>Id No.</th>
-            <th>Phone</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{loan.loanee.firstName} {loan.loanee.firstName}</td>
-            <td>{loan.loanee.idNumber}</td>
-            <td>{loan.loanee.phoneNumber}</td>
-          </tr>
-        </tbody>
-        </table>
-      </div> : <div>Not Available</div>}
-      <h1 className="text-sm font-medium text-orange-500 uppercase">
-        Loan Information
-      </h1>
-      <div className="overflow-x-auto">
-        <table className="table table-xs">
-
-        <thead>
-          <tr>
-            <th>Loan Amount</th>
-            <th>Date Borrowed</th>
-            <th>Loan Reason</th>
-            <th>Amount Paid</th>
-            <th>Date Paid</th>
-            <th>Loan Balance</th>
-
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{formatCurrency(loan.loanAmount).toString()}</td>
-            <td>{formatDate(loan.createdAt!)}</td>
-            <td>{loan.loanReason}</td>
-            <td>{formatCurrency(loan.amountPaid).toString()}</td>
-            {loan.receivedBy ? <td>{formatDate(loan.updatedAt!)}</td> : <td>Not Paid</td>}
-            <td>{formatCurrency(loan.loanAmount - loan.amountPaid).toString()}</td>
-          </tr>
-        </tbody>
-        </table>
-      </div>
-      
-      
-
-     
-
-      <h1 className="text-sm font-medium text-orange-500 uppercase py-1">
-        Served By
-      </h1>
-
-      {loan.servedBy ? <div className="overflow-x-auto">
-      <table className="table table-xs">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Phone</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{loan.servedBy.name}</td>
-            <td>{loan.servedBy.phone}</td>
-            <td>{loan.servedBy.email}</td>
-          </tr>
-        </tbody>
-      </table>
-      </div> : <div>Not Available</div>}
-
-      <h1 className="text-sm font-medium text-orange-500 uppercase py-1">
-        Payment Recipient Details
-      </h1>
-      
-      {loan.receivedBy ? <div className="overflow-x-auto">
-      <table className="table table-xs">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Phone</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{loan.servedBy.name}</td>
-            <td>{loan.servedBy.phone}</td>
-            <td>{loan.servedBy.email}</td>
-          </tr>
-        </tbody>
-      </table>
-      </div> : <div>Not Available</div>}
-    
-     
-      
-    </div>
-    <Link to={`/download/${loan._id}`} className="btn btn-primary">DownLoad Link</Link>
+      <Link to={`/download/${loan._id}`} className="btn btn-primary">
+        DownLoad Link
+      </Link>
     </>
   );
 }
