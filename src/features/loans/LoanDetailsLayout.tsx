5b321d003509a5d416592a78bac515677867e3eb
Brian-mula <mulatiwangilabrian@gmail.com>
2024-01-02 00:46:29 +0300

    modify ui display

--- src/features/loans/LoanDetailsLayout.tsx
+++ src/features/loans/LoanDetailsLayout.tsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import BackButton from "../../ui/BackButton";
-import ColumnText from "../../ui/ColumnText";
 import Loader from "../../ui/Loader";
 import { formatCurrency, formatDate } from "../../utils/helpers";
 import RepayLoanModal from "./RepayLoanModal";
@@ -28,62 +27,107 @@ export default function LoanDetailsLayout() {
 
         {loan.loanAmount !== loan.amountPaid && <RepayLoanModal />}
       </div>
-      <h1 className="text-lg font-medium text-orange-500 uppercase">
+      <h1 className="text-sm font-medium text-orange-500 uppercase">
+        Borrower Information
+      </h1>
+     {loan.loanee? <div className="overflow-x-auto">
+        <table className="table table-xs">
+        <thead>
+          <tr>
+            <th>Full Name</th>
+            <th>Id No.</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{loan.loanee.firstName} {loan.loanee.firstName}</td>
+            <td>{loan.loanee.idNumber}</td>
+            <td>{loan.loanee.phoneNumber}</td>
+          </tr>
+        </tbody>
+        </table>
+      </div> : <div>Not Available</div>}
+      <h1 className="text-sm font-medium text-orange-500 uppercase">
         Loan Information
       </h1>
+      <div className="overflow-x-auto">
+        <table className="table table-xs">
 
-      <div className="flex justify-between items-start my-2">
-      <ColumnText title="Date Borrowed" text={formatDate(loan.createdAt!)} />
-      {loan.receivedBy && <ColumnText title="Date Paid" text={formatDate(loan.updatedAt!)} />}
+        <thead>
+          <tr>
+            <th>Loan Amount</th>
+            <th>Date Borrowed</th>
+            <th>Loan Reason</th>
+            <th>Amount Paid</th>
+            <th>Date Paid</th>
+            <th>Loan Balance</th>
+
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{formatCurrency(loan.loanAmount).toString()}</td>
+            <td>{formatDate(loan.createdAt!)}</td>
+            <td>{loan.loanReason}</td>
+            <td>{formatCurrency(loan.amountPaid).toString()}</td>
+            {loan.receivedBy ? <td>{formatDate(loan.updatedAt!)}</td> : <td>Not Paid</td>}
+            <td>{formatCurrency(loan.loanAmount - loan.amountPaid).toString()}</td>
+          </tr>
+        </tbody>
+        </table>
       </div>
-      <div className="flex justify-between shadow p-3">
-        <div>
-        
-          <ColumnText title="Loan Amount" text={formatCurrency(loan.loanAmount).toString()} />
-          <ColumnText title="Loan Reason" text={loan.loanReason} />
-        </div>
-        <div>
-        
-          <ColumnText
-            title="Loan Balance"
-            text={formatCurrency(loan.loanAmount - loan.amountPaid).toString()}
-          />
-          <ColumnText title="Amount Paid" text={formatCurrency(loan.amountPaid).toString()} />
-        </div>
-      </div>
+      
+      
 
-      <h1 className="text-lg font-medium text-orange-500 uppercase">
-        Loanee Information
-      </h1>
-
-     { loan.loanee ?  <div className="shadow grid grid-cols-2 md:grid-cols-4 gap-8 p-4 mt-2">
-        <ColumnText title="First name" text={loan.loanee.firstName} />
-        <ColumnText title="Last name" text={loan.loanee.lastName} />
-        <ColumnText title="Id Number" text={loan.loanee.idNumber} />
-        <ColumnText title="Phone Number" text={loan.loanee.phoneNumber} />
-      </div> : <div>Not Available</div>}
-      <h1 className="text-lg font-medium text-orange-500 uppercase py-1">
-        Agent Information
-      </h1>
-
-      {loan.servedBy? <div className="shadow grid grid-cols-2 md:grid-cols-4 gap-8 p-4 mt-2">
-        <ColumnText title="Full Name" text={loan.servedBy.name} />
-        <ColumnText title="Phone Number" text={loan.servedBy.phone!} />
-        <ColumnText title="Email" text={loan.servedBy.email} />
-      </div> : <div>Not Available</div>}
-
-     {loan.receivedBy && <>
      
-      <h1 className="text-lg font-medium text-orange-500 uppercase py-1">
+
+      <h1 className="text-sm font-medium text-orange-500 uppercase py-1">
+        Served By
+      </h1>
+
+      {loan.servedBy ? <div className="overflow-x-auto">
+      <table className="table table-xs">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{loan.servedBy.name}</td>
+            <td>{loan.servedBy.phone}</td>
+            <td>{loan.servedBy.email}</td>
+          </tr>
+        </tbody>
+      </table>
+      </div> : <div>Not Available</div>}
+
+      <h1 className="text-sm font-medium text-orange-500 uppercase py-1">
         Payment Recipient Details
       </h1>
-
-      {loan.receivedBy ? <div className="shadow grid grid-cols-2 md:grid-cols-4 gap-8 p-4 mt-2">
-        <ColumnText title="Full Name" text={loan.receivedBy.name} />
-        <ColumnText title="Phone Number" text={loan.receivedBy.phone!} />
-        <ColumnText title="Email" text={loan.receivedBy.email} />
+      
+      {loan.receivedBy ? <div className="overflow-x-auto">
+      <table className="table table-xs">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{loan.servedBy.name}</td>
+            <td>{loan.servedBy.phone}</td>
+            <td>{loan.servedBy.email}</td>
+          </tr>
+        </tbody>
+      </table>
       </div> : <div>Not Available</div>}
-     </>}
+    
      
       
     </div>
