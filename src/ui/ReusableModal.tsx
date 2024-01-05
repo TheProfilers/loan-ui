eb265237958d69a3b5122668c7999ae8b8babc09
Brian-mula <mulatiwangilabrian@gmail.com>
2024-01-05 15:32:09 +0300

    modified reusable modal

--- src/ui/ReusableModal.tsx
+++ src/ui/ReusableModal.tsx
@@ -1,17 +1,57 @@
-import { ReactNode, useRef } from 'react';
+import { ReactNode, forwardRef, useImperativeHandle, useRef } from 'react';
 
-export default function ReusableModal({children,title}:{children:ReactNode,title:string}) {
-    const newLoanModal = useRef<HTMLDialogElement>(null);
+// export default function ReusableModal({children,title}:{children:ReactNode,title:string}) {
+//     const newLoanModal = useRef<HTMLDialogElement>(null);
+//   const openModal = () => {
+//     newLoanModal.current?.showModal();
+//   };
+//   const closeModal = () => {
+//     newLoanModal.current?.close();
+//   };
+//   return (
+//     <>
+    
+// <button type='button' className="btn bg-green-600 hover:bg-green-500 text-white font-bold" onClick={openModal}>{title}</button>
+// <dialog ref={newLoanModal} className="modal">
+//   <div className="modal-box">
+//     <div className="flex justify-end">
+//       <button type="button" onClick={closeModal} className="text-2xl cursor-pointer">
+//         X
+//       </button>
+//     </div>
+//     {children}
+   
+//   </div>
+// </dialog>
+//     </>
+//   )
+// }
+
+interface NewLoanModalProps {
+  title:string;
+  children:ReactNode;
+}
+
+export interface NewLoanModalPropsRef {
+openModal:()=>void;
+closeModal:()=>void;
+}
+
+const NewLoanReusableModal = forwardRef<NewLoanModalPropsRef,NewLoanModalProps>(({children,title},ref) => {
+  const newLoanModal = useRef<HTMLDialogElement>(null);
   const openModal = () => {
     newLoanModal.current?.showModal();
   };
   const closeModal = () => {
     newLoanModal.current?.close();
   };
+  useImperativeHandle(ref, () => ({
+    openModal,
+    closeModal,
+  }));
   return (
     <>
-    
-<button type='button' className="btn bg-green-600 hover:bg-green-500 text-white font-bold" onClick={openModal}>{title}</button>
+    <button type='button' className="btn bg-green-600 hover:bg-green-500 text-white font-bold" onClick={openModal}>{title}</button>
 <dialog ref={newLoanModal} className="modal">
   <div className="modal-box">
     <div className="flex justify-end">
@@ -25,53 +65,5 @@ export default function ReusableModal({children,title}:{children:ReactNode,title
 </dialog>
     </>
   )
-}
-
-// type ReusableModalProps = {
-//   children: ReactNode;
-//   title: string;
-// };
-
-// const ReusableModalClosabe = forwardRef(function ReusableModal({ children, title }: ReusableModalProps, ref) {
-//   const newLoanModal = useRef<HTMLDialogElement>(null);
-
-//   // Expose the closeModal function via the ref
-//   useImperativeHandle(ref, function () {
-//     return {
-//       openModal: function () {
-//         newLoanModal.current?.showModal();
-//       },
-//       closeModal: function () {
-//         newLoanModal.current?.close();
-//       },
-//     };
-//   });
-
-//   return (
-//     <>
-//       <button
-//         type="button"
-//         className="btn bg-green-600 hover:bg-green-500 text-white font-bold"
-//         onClick={() => ref?.current?.openModal()}
-//       >
-//         {title}
-//       </button>
-//       <dialog ref={newLoanModal} className="modal">
-//         <div className="modal-box">
-//           <div className="flex justify-end">
-//             <button
-//               type="button"
-//               onClick={() => ref?.current?.closeModal()}
-//               className="text-2xl cursor-pointer"
-//             >
-//               X
-//             </button>
-//           </div>
-//           {children}
-//         </div>
-//       </dialog>
-//     </>
-//   );
-// });
-
-// export default ReusableModalClosabe;
+});
+export default NewLoanReusableModal;
