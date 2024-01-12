d7a2ee11f9b48b3076421f72b0b4379a42518bce
Brian-mula <mulatiwangilabrian@gmail.com>
2024-01-13 00:37:48 +0300

    loan requests

--- src/services/apiStock.ts
+++ src/services/apiStock.ts
@@ -55,3 +55,37 @@ export async function assignStockToAgent(stock:IStock){
         throw new Error(error.message);
       }
 }
+
+export interface IStockRequest{
+  id:string,
+  amountRequested:number,
+  requester:string,
+  reciever:string,
+}
+
+export async function requestMoreStock(stockRequest:IStockRequest){
+    try {
+        const response = await fetch(`${BASE_URL}stock/request/${stockRequest.id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${JSON.parse(localStorage.getItem("token")!)}`,
+          },
+          body: JSON.stringify(stockRequest),
+        });
+        const data = await response.json();
+        if (data.statusCode === 500) {
+          throw new Error(data.message);
+        }
+        if (response.status === 401) {
+          throw new Error("You are not authorized to perform this action");
+        }
+        if (data.statusCode) {
+          throw new Error(data.message);
+        }
+        return data;
+      }catch (error: any) {
+        throw new Error(error.message);
+      }
+
+}
