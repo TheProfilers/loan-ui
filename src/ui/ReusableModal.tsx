968393b430f36c802c38c8c2ac72ab937c6bd557
Brian-mula <mulatiwangilabrian@gmail.com>
2024-01-12 22:40:51 +0300

    added assign stock

--- src/ui/ReusableModal.tsx
+++ src/ui/ReusableModal.tsx
@@ -1,31 +1,6 @@
 import { ReactNode, forwardRef, useImperativeHandle, useRef } from 'react';
 
-// export default function ReusableModal({children,title}:{children:ReactNode,title:string}) {
-//     const newLoanModal = useRef<HTMLDialogElement>(null);
-//   const openModal = () => {
-//     newLoanModal.current?.showModal();
-//   };
-//   const closeModal = () => {
-//     newLoanModal.current?.close();
-//   };
-//   return (
-//     <>
-    
-// <button type='button' className="btn bg-green-600 hover:bg-green-500 text-white font-bold" onClick={openModal}>{title}</button>
-// <dialog ref={newLoanModal} className="modal">
-//   <div className="modal-box">
-//     <div className="flex justify-end">
-//       <button type="button" onClick={closeModal} className="text-2xl cursor-pointer">
-//         X
-//       </button>
-//     </div>
-//     {children}
-   
-//   </div>
-// </dialog>
-//     </>
-//   )
-// }
+
 
 interface NewLoanModalProps {
   title:string;
