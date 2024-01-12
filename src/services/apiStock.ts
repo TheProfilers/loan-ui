e6cb5fdc27cfa4f73143992acbbc6d1fecf91fb8
Brian-mula <mulatiwangilabrian@gmail.com>
2024-01-12 23:06:49 +0300

    added new stock capability

--- src/services/apiStock.ts
+++ src/services/apiStock.ts
@@ -25,3 +25,33 @@ export async function getTodayIndividualStock(id:string){
         throw new Error(error.message);
       }
 }
+export interface IStock{
+  amount:number,
+  belongsTo:string,
+  status:string,
+}
+export async function assignStockToAgent(stock:IStock){
+    try {
+        const response = await fetch(`${BASE_URL}stock/new`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${JSON.parse(localStorage.getItem("token")!)}`,
+          },
+          body: JSON.stringify(stock),
+        });
+        const data = await response.json();
+        if (data.statusCode === 500) {
+          throw new Error(data.message);
+        }
+        if (response.status === 401) {
+          throw new Error("You are not authorized to perform this action");
+        }
+        if (data.statusCode) {
+          throw new Error(data.message);
+        }
+        return data;
+      }catch (error: any) {
+        throw new Error(error.message);
+      }
+}
