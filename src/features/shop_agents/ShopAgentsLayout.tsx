ba7bb5fc2870c46bfaa699784a23796f4a2495ec
Brian-mula <mulatiwangilabrian@gmail.com>
2024-01-13 11:15:33 +0300

    performed authorization

--- src/features/shop_agents/ShopAgentsLayout.tsx
+++ src/features/shop_agents/ShopAgentsLayout.tsx
@@ -1,13 +1,15 @@
+import { useAuth } from "../../context/AuthContext";
 import BackButton from "../../ui/BackButton";
 import NewShopAgentModal from "./NewShopAgentModal";
 import ShopAgentDisplay from "./ShopAgentDisplay";
 
 export default function ShopAgentsLayout() {
+  const {storedUser} = useAuth();
   return (
     <>
     <div className="flex justify-between">
         <BackButton/>
-        <NewShopAgentModal/>
+        {storedUser?.role ==='admin' && <NewShopAgentModal/>}
     </div>
     <ShopAgentDisplay/>
     </>
