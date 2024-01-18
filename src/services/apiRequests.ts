a680adac7bec0d67311b9850764ef8f9663bfda5
Brian-mula <mulatiwangilabrian@gmail.com>
2024-01-18 23:40:31 +0300

    agent borrowing

--- src/services/apiRequests.ts
+++ src/services/apiRequests.ts
@@ -101,3 +101,27 @@ export async function approveStocRequest(id:string){
         throw new Error(error.message);
       }
 }
+export async function declineStocRequest(id:string){
+    try {
+        const response = await fetch(`${BASE_URL}requests/reject/${id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${JSON.parse(localStorage.getItem("token")!)}`,
+          },
+        });
+        const data = await response.json();
+        if (data.statusCode === 500) {
+          throw new Error(data.message);
+        }
+        if (response.status === 401) {
+          throw new Error("You are not authorized to perform this action");
+        }
+        if (data.statusCode) {
+          throw new Error(data.message);
+        }
+        return data;
+      }catch (error: any) {
+        throw new Error(error.message);
+      }
+}
