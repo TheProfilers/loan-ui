1f5d4a5788633e711bd54f04b628c6dc27350632
Brian-mula <mulatiwangilabrian@gmail.com>
2024-01-22 16:05:31 +0300

    completed stock requests

--- src/features/assigne_stock/AssignStockLayout.tsx
+++ src/features/assigne_stock/AssignStockLayout.tsx
@@ -1,15 +1,20 @@
+import { useAuth } from "../../context/AuthContext";
 import BackButton from "../../ui/BackButton";
 import AgentsList from "./AgentsList";
 
 export default function AssignStockLayout() {
+  const { storedUser } = useAuth();
   return (
-   <>
-   <div className="flex justify-between items-start pt-2 mb-4">
-    <BackButton />
-    <h3 className="text-orange-500 font-medium text-lg">Choose Agent To Assign Stock</h3>
-
-   </div>
-   <AgentsList />
-   </>
-  )
+    <>
+      <div className="flex justify-between items-start pt-2 mb-4">
+        <BackButton />
+        {storedUser?.role === "admin" && (
+          <h3 className="text-orange-500 font-medium text-lg">
+            Choose Agent To Assign Stock
+          </h3>
+        )}
+      </div>
+      <AgentsList />
+    </>
+  );
 }
