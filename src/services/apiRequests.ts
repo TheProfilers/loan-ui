5051fab0d5f28d8547ea08cd93b13881d7dfd4b9
Brian-mula <mulatiwangilabrian@gmail.com>
2024-01-23 00:34:04 +0300

    completed requests

--- src/services/apiRequests.ts
+++ src/services/apiRequests.ts
@@ -53,6 +53,30 @@ export async function getStockTodayRequests(id:string){
       }
 }
 
+export async function getTodayRequesterRequests(id:string){
+  try {
+      const response = await fetch(`${BASE_URL}requests/requester/${id}`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${JSON.parse(localStorage.getItem("token")!)}`,
+        },
+      });
+      const data = await response.json();
+      if (data.statusCode === 500) {
+        throw new Error(data.message);
+      }
+      if (response.status === 401) {
+        throw new Error("You are not authorized to perform this action");
+      }
+      if (data.statusCode) {
+        throw new Error(data.message);
+      }
+      return data;
+    }catch (error: any) {
+      throw new Error(error.message);
+    }
+}
+
 export async function getStockRequests(){
     try {
         const response = await fetch(`${BASE_URL}requests`, {
