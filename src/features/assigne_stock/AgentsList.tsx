c9d4c7d362e3e9f7ef3bc4716d7971155243f629
Brian-mula <mulatiwangilabrian@gmail.com>
2024-01-22 15:54:38 +0300

    Modified stock requests

---
+++ src/features/assigne_stock/AgentsList.tsx
@@ -0,0 +1,72 @@
+import { BsFillEyeFill, BsTrash3 } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import Loader from '../../ui/Loader';
+import { useDeleteShopAgent } from '../shop_agents/useDeleteShopAgent';
+import { useShopAgents } from '../shop_agents/useShopAgents';
+
+export default function AgentsList() {
+    const {data,isLoading,error} = useShopAgents();
+    const {mutate,isPending}= useDeleteShopAgent();
+
+    const handleDelete = (id:string) => {
+        mutate(id);
+        
+    }
+
+    if(isLoading) {
+        return <Loader/>
+    }
+    if(error) {
+        Swal.fire({
+            title:'Error',
+            text:error.message,
+            icon:'error'
+        })
+        
+    }
+    if(data?.length === 0) {
+        return (
+            <div className='h-screen flex flex-col justify-center items-center'>
+                <h1 className='text-2xl'>No Agents Found</h1>
+            </div>
+        )
+    }
+  return (
+    <div className="overflow-x-auto">
+      <table className="table table-xs">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+        
+         {
+            data?.map((agent,index) => (
+                <tr className="" key={index} >
+                <th>{index + 1}</th>
+                <td>{agent.name}</td>
+                <td>{agent.phone}</td>
+                <td>
+                  <div className="flex space-x-1">
+                    <Link to={`/stock/${agent._id}`} className="btn btn-sm btn-square bg-green-500 hover:bg-green-400 text-white">
+                      <BsFillEyeFill />
+                    </Link>
+                    <button disabled={isPending} onClick={()=>handleDelete(agent._id!)} className="btn btn-sm btn-square btn-warning text-white">
+                      <BsTrash3 />
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))
+         }
+        </tbody>
+      </table>
+    </div>
+  )
+}
