b3c1e26cc8e764bfb51d3751646028dbe0a49fb5
Brian-mula <mulatiwangilabrian@gmail.com>
2024-01-22 15:54:43 +0300

    Modified stock requests

--- src/features/assigne_stock/AssignStockLayout.tsx
+++ src/features/assigne_stock/AssignStockLayout.tsx
@@ -1,6 +1,15 @@
+import BackButton from "../../ui/BackButton";
+import AgentsList from "./AgentsList";
 
 export default function AssignStockLayout() {
   return (
-    <div>AssignStockLayout</div>
+   <>
+   <div className="flex justify-between items-start pt-2 mb-4">
+    <BackButton />
+    <h3 className="text-orange-500 font-medium text-lg">Choose Agent To Assign Stock</h3>
+
+   </div>
+   <AgentsList />
+   </>
   )
 }
