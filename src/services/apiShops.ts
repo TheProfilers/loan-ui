727f291f4d344024926f2edb7aa28200228dc47d
Brian-mula <mulatiwangilabrian@gmail.com>
2024-01-26 02:15:54 +0300

    shop display

--- src/services/apiShops.ts
+++ src/services/apiShops.ts
@@ -29,3 +29,27 @@ export async function newShop(shop:ShopType){
       }
 
 }
+
+export async function getShops(){
+    try {
+        const response = await fetch(`${BASE_URL}shop`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${JSON.parse(localStorage.getItem("token")!)}`,
+          },
+        });
+        const data = await response.json();
+        if (data.statusCode === 500) {
+          throw new Error(data.message);
+        }
+        if (response.status === 401) {
+          throw new Error("You are not authorized to perform this action");
+        }
+        if (data.statusCode) {
+          throw new Error(data.message);
+        }
+        return data;
+      }catch (error: any) {
+        throw new Error(error.message);
+      }
+}
