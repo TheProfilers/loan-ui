8ab5d6380b82ae350ff0fe0321aa2cbb572cda64
Brian-mula <mulatiwangilabrian@gmail.com>
2024-01-28 14:21:42 +0300

    new float

--- src/features/sagents/SAgentDetailsLayout.tsx
+++ src/features/sagents/SAgentDetailsLayout.tsx
@@ -1,6 +1,16 @@
+import BackButton from "../../ui/BackButton";
+import NewAgentFloatModal from "./NewAgentFloatModal";
 
 export default function SAgentDetailsLayout() {
   return (
-    <div>SAgentDetailsLayout</div>
-  )
+    <>
+      <BackButton />
+
+      <div className="flex justify-between items-start mt-2">
+        <h1 className="text-lg font-medium text-orange-600">FLOAT</h1>
+
+        <NewAgentFloatModal />
+      </div>
+    </>
+  );
 }
