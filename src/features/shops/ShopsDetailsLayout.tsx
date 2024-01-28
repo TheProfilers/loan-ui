a59ad410f76dabf87b2138115a2677f12266b4bb
Brian-mula <mulatiwangilabrian@gmail.com>
2024-01-28 08:34:22 +0300

    added new agent capability

--- src/features/shops/ShopsDetailsLayout.tsx
+++ src/features/shops/ShopsDetailsLayout.tsx
@@ -1,14 +1,19 @@
 import BackButton from "../../ui/BackButton";
+import NewSAgents from "../sagents/NewSAgents";
+import SAgents from "../sagents/SAgents";
 
 export default function ShopsDetailsLayout() {
   return (
     <>
-    <div className="flex justify-between items-start mt-3">
+    <div className="flex justify-between items-start mt-3 mb-2">
         <div className="flex space-x-2">
             <BackButton />
             <h3>Agents</h3>
         </div>
+        <NewSAgents/>
     </div>
+
+    <SAgents/>
     </>
   )
 }
