db28e544510b9c90630b8097de32d86912435fe6
Brian-mula <mulatiwangilabrian@gmail.com>
2024-01-30 16:28:18 +0300

    added current float balances

--- src/features/sagents/FloatDisplay.tsx
+++ src/features/sagents/FloatDisplay.tsx
@@ -1,5 +1,6 @@
 import Loader from "../../ui/Loader"
 import FloatItem from "./FloatItem"
+import ReusableFloatItem from "./ReusableFloatItem"
 import { useShopAgentFloat } from "./useShopAgentFloat"
 
 export default function FloatDisplay() {
@@ -17,11 +18,26 @@ export default function FloatDisplay() {
     console.log(floats);
   return (
     <>
+    <h1 className="font-medium text-lg text-gray-500">Assigned Float</h1>
    {
     floats.map((float,index)=>(
         <FloatItem key={index} float={float}/>
     ))
    }
+   <h1 className="font-medium text-lg text-gray-500">Current Float</h1>
+   {
+    floats.map((float,index)=>(
+        <ReusableFloatItem key={index} currency={float.currency} amount={float.totalAmount}/>
+    ))
+   }
+
+<h1 className="font-medium text-lg text-gray-500">Float Balance</h1>
+{
+    floats.map((float,index)=>(
+        <ReusableFloatItem key={index} currency={float.currency} amount={float.amount - float.totalAmount}/>
+    ))
+   }
+
     </>
   )
 }
